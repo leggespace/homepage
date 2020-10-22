92709a10428fcc3073b63550f4be058db9a5142a
Rikard Legge <rikard.legge@gmail.com>
2020-10-23 01:49:52 +0200

    Feature check

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
-document.querySelectorAll(".waypoint")
-  .forEach(element=>element.classList.add("waypoint-hidden"));
+if(typeof(IntersectionObserver) !== "undefined") {
+  document.querySelectorAll(".waypoint")
+    .forEach(element => element.classList.add("waypoint-hidden"));
 
-waypoint('#waypoint-1', 'fadeInLeft', 0.75);
-waypoint('#waypoint-2', 'fadeInUp', 0);
-waypoint('#waypoint-3', 'fadeInUp', 0);
-waypoint('#waypoint-4', 'fadeInUp', 0);
-waypoint('#waypoint-5', 'fadeInUp', 0.5);
+  waypoint('#waypoint-1', 'fadeInLeft', 0.75);
+  waypoint('#waypoint-2', 'fadeInUp', 0);
+  waypoint('#waypoint-3', 'fadeInUp', 0);
+  waypoint('#waypoint-4', 'fadeInUp', 0);
+  waypoint('#waypoint-5', 'fadeInUp', 0.5);
+}
 
 function waypoint(selector, animation, offset) {
   const options = {
     threshold: [offset],
   };
-  respondToVisibility(selector, (element)=>{
+  respondToVisibility(selector, (element) => {
     element.classList.add('animated');
     element.classList.remove('waypoint-hidden');
     element.classList.add(animation);
@@ -23,7 +25,7 @@ function respondToVisibility(selector, callback, options) {
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-      if(entry.intersectionRatio > 0){
+      if (entry.intersectionRatio > 0) {
         callback(element);
       }
     });
